244b22d4c4fed360237e07dcae2b8a4e84d693a6
hezkun <38609632+hezkun@users.noreply.github.com>
2018-04-27 10:26:41 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-echo "欢迎使用一键搭建"
-echo "即将搭建的是4.26版本"
-echo "准备开始安装"
-read -p "回车后开始安装"
-echo "请输入你的内网ip" 
+echo "请记住连接信息"
 read -p "内网ip： " ip
 cp /root/winnerpubg/restart.sh /root/restart.sh
 chmod +x restart.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
-
-echo "ss搭建成，请记住连接信息"
-read -p "记住了吗？任意键继续" 
 
 curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
 source ~/.bash_profile
@@ -28,8 +21,8 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/PUBG-Radar-Onekey.git
-cd PUBG-Radar-Onekey/
+git clone https://github.com/hezkun/pubgkradar2.git
+cd pubgkradar2/
 npm i
 npm i -g pino
 npm install -g forever
